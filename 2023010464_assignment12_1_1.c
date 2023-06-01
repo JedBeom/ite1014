c5b32a815dd5a281229ab9ca07d6a1ca42045861
2023010464 <jedbeom@hanyang.ac.kr>
2023-06-01 20:37:31 +0900

    Add comments to 12_1_1.c

--- 2023010464_assignment12_1_1.c
+++ 2023010464_assignment12_1_1.c
@@ -24,7 +24,7 @@ int solution(int n, int k) {
 
 	for (; cursor <= len; cursor++) {
 		if (str[cursor] == '0' || str[cursor] == '\0') {
-			int num = atoiRange(str, start, cursor);
+			int num = atoiRange(str, start, cursor); // num = int(str[start:cursor])
 			if (isPrime(num)) {
 				primeCount++;
 			}
@@ -53,9 +53,10 @@ int atoiRange(const char str[], int start, int end) {
 
 	strncpy(substr, str+start, end-start); // strncpy does not insert null terminator
 	substr[end-start] = '\0'; // insert '\0' to the end of substr
+							  // substr = str[start:end]
 
 	int len = strlen(substr), num = 0;
-	for (int i=0; i<len; i++) {
+	for (int i=0; i<len; i++) { // num = int(substr)
 		num += (substr[i]-'0') * (int)pow(10, len-i-1);
 	}
 
@@ -65,15 +66,15 @@ int atoiRange(const char str[], int start, int end) {
 int convertDigit(char* num, int n, int k) {
 	int index = 0, tmp;
 
-	while (n>0) {
+	while (n>0) { // add remainders to num
 		num[index] = n%k +'0';
 		n /= k;
 		index++;
 	}
-	num[index] = '\0';
+	num[index] = '\0'; // add null terminator to the end of num
 
 	int len = strlen(num);
- 	for (index = 0; index < (len/2); index++) {
+ 	for (index = 0; index < (len/2); index++) { // num.reverse()
 		tmp = num[index];
 		num[index] = num[len-index-1];
 		num[len-index-1] = tmp;
